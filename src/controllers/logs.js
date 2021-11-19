c3748f5b06b9cc417b07c6935e21f758b4bf61b8
Kristijan <kristijan.pavlevski@outlook.com>
2021-11-19 13:10:05 +0100

    Add pagination to logs page.

--- src/controllers/logs.js
+++ src/controllers/logs.js
@@ -2,10 +2,25 @@ const CertificateLog = require('../models/certificateLog');
 
 exports.getLogs = async (req, res, next) => {
     try {
-        const logs = await CertificateLog.find({});
-        res.render('logs/index', {
-            logs,
-            page: 'logs'
+        const pageSize = +req.query.pageSize;
+        const currentPage = +req.query.currentPage;
+        const logsQuery = CertificateLog.find({});
+
+        const numOfLogs = await CertificateLog.countDocuments().exec();
+
+        if (req.query.currentPage && req.query.pageSize > 0) {
+            logsQuery
+                .skip((currentPage - 1) * pageSize)
+                .limit(pageSize)
+        }
+
+        logsQuery.then(logs => {
+            res.render('logs/index', {
+                logs,
+                page: 'logs',
+                pages: Math.ceil(numOfLogs / pageSize),
+                currentPage
+            });
         });
     } catch (err) {
         req.flash('error', `Error: ${err}`);
